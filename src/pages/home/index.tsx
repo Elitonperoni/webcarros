1511f36aabff46e5862aa72500760c6fc85ae845
eliton <eliton_peroni@hotmail.com>
2024-12-26 12:18:34 -0300

    ...

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,49 +1,104 @@
+import { useState, useEffect } from 'react'
 import { Container } from "../../components/container";
+import {
+  collection,
+  query,
+  getDocs,
+  orderBy
+} from 'firebase/firestore';
+import { db } from '../../services/firebaseConnection'
+
+interface CarProps {
+  id: string;
+  name: string;
+  year: string;
+  uid: string;
+  price: string | number;
+  city: string;
+  km: string;
+  images: CarImageProps[]
+}
+
+interface CarImageProps {
+  name: string;
+  uid: string;
+  url: string;
+}
 
 export function Home() {
-    return (
-      <Container>
-        <section className="bg-white p-4 rounded-lg w-full max-w-3xl mx-auto flex justify-center items-center gap-2">
-          <input
-            className="2-full border-2 rounded-lg h-9 px-3 outline-none"
-            placeholder="Digite o nome do carro..."
-          />          
-          <button
-            className="bg-red-500 h-9 px-8 rounded-lg text-white font-medium text-lg"
-          >
-            Buscar
-          </button>
-        </section>
+  const [cars, setCars] = useState<CarProps[]>([]);
 
-        <h1 className="font-bold text-center mt-6 text-2xl mb-4">
-          Carros novos e usados em todo o Brasil
-        </h1>
+  useEffect(() => {
+    function loadCars(){
+      const carRef = collection(db, "cars")
+      const queryRef = query(carRef, orderBy("created", "desc"))
+      
+      getDocs(queryRef)
+      .then((snapshot) => {
+        let listcars = [] as CarProps[];
 
-        <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+        snapshot.forEach(doc => {
+          listcars.push({
+            id: doc.id,
+            name: doc.data().name,
+            year: doc.data().year,
+            km: doc.data().km,
+            city: doc.data().city,
+            price: doc.data().price,
+            images: doc.data().images,
+            uid: doc.data().uid
+          })
+        })
+       setCars(listcars)
+      })
+    }
+    loadCars()
+  }, [])
 
-          <section className="w-full bg-white rounded-lg">
-            <img 
-              className="w-full rounded-lg mb-2 max-h-90 hover:scale-105 transition-all"
-              src="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2024/202409/20240902/porsche-911-4-0-24v-h6-gasolina-gt3-pdk-wmimagem18594422136.webp?s=fill&w=1920&h=1440&q=75"
-              alt="Carro" />
-              <p className="font-bold mt-1 mb-2 px-2">Porsche 911 GT3</p>
+  return (
+    <Container>
+      <section className="bg-white p-4 rounded-lg w-full max-w-3xl mx-auto flex justify-center items-center gap-2">
+        <input
+          className="2-full border-2 rounded-lg h-9 px-3 outline-none"
+          placeholder="Digite o nome do carro..."
+        />
+        <button
+          className="bg-red-500 h-9 px-8 rounded-lg text-white font-medium text-lg"
+        >
+          Buscar
+        </button>
+      </section>
 
-              <div className="flex flex-col px-2">
-                <span className="text-zinc-700 mb-6">Ano 2016/2016 | 23.000 km</span>
-                <strong className="text-black font-medium text-xl">R$ 1.900.000,00</strong>
-              </div>
+      <h1 className="font-bold text-center mt-6 text-2xl mb-4">
+        Carros novos e usados em todo o Brasil
+      </h1>
 
-              <div className="w-full h-px bg-slate-200 my-2"></div>
+      <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
 
-              <div className="px-2 pb-2">
-                <span className="text-zinc-700">
-                  Curitiba - PR
-                </span>
-              </div>
-          </section>                
+       {cars.map(car => (
+         <section key={car.id} className="w-full bg-white rounded-lg">
+         <img
+           className="w-full rounded-lg mb-2 max-h-90 hover:scale-105 transition-all"
+           src={car.images[0].url}
+           alt="Carro" />
+         <p className="font-bold mt-1 mb-2 px-2">{car.name}</p>
 
-        </main>
-      </Container>
-    );
-  }
-  
+         <div className="flex flex-col px-2">
+           <span className="text-zinc-700 mb-6">Ano {car.year} | {car.km} km</span>
+           <strong className="text-black font-medium text-xl">R$ {car.price}</strong>
+         </div>
+
+         <div className="w-full h-px bg-slate-200 my-2"></div>
+
+         <div className="px-2 pb-2">
+           <span className="text-zinc-700">
+             {car.city}
+           </span>
+         </div>
+       </section>
+       ))}
+
+      </main>
+    </Container>
+  );
+}
